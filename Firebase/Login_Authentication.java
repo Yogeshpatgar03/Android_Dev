b4c0b230267ebca4cbfc6d8d088bd00a07ccbde0
YOGESH PATGAR <117726602+Yogeshpatgar03@users.noreply.github.com>
2023-10-21 22:56:24 +0530

    Add files via upload

---
+++ Firebase/Login_Authentication.java
@@ -0,0 +1,73 @@
+package com.example.shopit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+
+    EditText email,password;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        auth = FirebaseAuth.getInstance();
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+    }
+
+    public void signup(View view)
+    {
+        startActivity(new Intent(Login.this, Register.class));
+    }
+    public void signin(View view){
+
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
+
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(this, "Enter Email Address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)) {
+            Toast.makeText(this, "Enter Password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (userPassword.length() <6) {
+            Toast.makeText(this, "Wrong Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        auth.signInWithEmailAndPassword(userEmail, userPassword)
+                .addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+
+                            Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, MainActivity.class));
+
+                        } else {
+                            Toast.makeText(Login.this, "Error" + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
+    }
